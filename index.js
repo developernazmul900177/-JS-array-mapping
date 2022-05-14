24f6cfd837a214411729e9acf85e38910381b4c1
Nazmul <nazmulislamn95@gmail.com>
2022-05-14 16:45:46 +0600

    JS array maping

---
+++ index.js
@@ -0,0 +1,16 @@
+
+let numbers = [1, 2, 3, 4, 5, 6];
+
+const mynum = numbers.map(function (item) {
+    return item * item;
+});
+console.log(mynum);
+
+
+//Array Maping
+
+
+const mynewNum = numbers.map(item => {
+    return item * item;
+})
+console.log(mynewNum);
